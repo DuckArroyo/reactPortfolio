d27298bfae65718220af81b802035b64d5166c94
Rafael "Duck" Arroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-12-16 16:24:32 -0800

    created contact form

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React, { useState } from "react";
 // import './App.css'; unused now - moved file to assets folder.
 import Nav from "./components/Nav";
 import About from "./components/About";
+import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 function App() {
-
   return (
     <div>
       <Nav></Nav>
       <main>
         <About></About>
+        <Contact></Contact>
       </main>
       <Footer></Footer>
     </div>
