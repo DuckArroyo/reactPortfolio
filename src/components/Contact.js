ed2d9cd056299a3e3c9c2c10570d7b3c95680526
Rafael <developerduckarroyo@gmail.com>
2021-12-18 09:38:25 -0800

    added fetch function, copied from tutorial

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -59,7 +59,16 @@ export default function Contact() {
       email: email.value,
       message: message.value,
     };
-
+    let response = await fetch("http://localhost:5000/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(details),
+    });
+    setStatus("Submit");
+    let result = await response.json();
+    alert(result.status);
   }
 
   return (
