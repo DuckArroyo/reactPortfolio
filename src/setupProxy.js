8f1f2cb0e25227ce298742288062a3ad740f277a
Rafael "Duck" Arroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-12-21 23:49:21 -0800

    link typo

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -12,3 +12,9 @@ module.exports = function (app) {
 
 //REMOVED from package JSON because it breaks the app
 // "proxy": "http://localhost:5002",
+
+// Setup a contact form in React that sends email via Node.js
+// https://w3collective.com/react-contact-form/
+
+// Proxying API Requests in Development
+// https://create-react-app.dev/docs/proxying-api-requests-in-development/
