0313f9af9ba0a86b763cfa6c2fae1aaccc3e1283
Rafael "Duck" Arroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-12-21 18:53:07 -0800

    editing paths

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 // import './App.css'; unused now - moved file to assets folder.
-import Nav from "./components/Nav";
-import Home from "./components/About";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Portfolio from "./components/Portfolio";
-import Resume from "./components/Resume";
-import FiveYrPlan from "./components/FiveYrPlan";
-import Footer from "./components/Footer";
+import Nav from './components/Nav';
+import Home from './components/About';
+import About from './components/About';
+import Contact from './components/Contact';
+import Portfolio from './components/Portfolio';
+import Resume from './components/Resume';
+import FiveYrPlan from './components/FiveYrPlan';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App" style={{height: '100vh'}}>
+    <div className='App' style={{ height: '100vh' }}>
       <Nav />
       <div id='container'>
-      <Routes>
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/resume" element={<Resume />} />
-        <Route path="/fiveyear" element={<FiveYrPlan />} />
-        <Route path="*" element={<Home />} />
-      </Routes>
+        <Routes>
+          <Route path='/challenge20/about' element={<About />} />
+          <Route path='/challenge20/contact' element={<Contact />} />
+          <Route path='/challenge20/portfolio' element={<Portfolio />} />
+          <Route path='/resume' element={<Resume />} />
+          <Route path='/fiveyear' element={<FiveYrPlan />} />
+          <Route path='*' element={<Home />} />
+        </Routes>
       </div>
       <Footer />
-
     </div>
   );
 }
