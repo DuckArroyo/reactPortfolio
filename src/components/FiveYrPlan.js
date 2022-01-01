609028deb944ecbb8ec09e661cb1f3fd9d1b67c0
Rafael <developerduckarroyo@gmail.com>
2022-01-01 00:46:15 -0800

    updated 5 year plan.

--- src/components/FiveYrPlan.js
+++ src/components/FiveYrPlan.js
@@ -1,12 +1,41 @@
-import React from 'react'
+import React from 'react';
 
 export default function FiveYrPlan() {
-    return (
-        <section>
-        <h2>In five years</h2>
-        <div id="content">
-          <p>In 5 years I would like to be in Japan</p>
-        </div>
-      </section>
-    )
+  return (
+    <section>
+      <h2>In five years</h2>
+      <div id='content'>
+        <p>All Five year plans start in the present. My goals for 2022 are:</p>
+        <br />
+        <ul>
+          <li>
+            Practice Developing
+            <strong>Goal: Create 12 applications.</strong>
+          </li>
+          <li>
+            Learn to properly jump rope
+            <strong>Goal: 50 continuous jumps+</strong>
+          </li>
+          <li>
+            Practice Japanese
+            <strong>Goal: create a video skit.</strong>
+          </li>
+          <li>
+            Blog
+            <strong>Goal: Write 52 blogs</strong>
+          </li>
+          <li>
+            Vlog
+            <strong>Goal: Create 12 videos</strong>
+          </li>
+          <li>
+            Fun goal. Reach 500,000,000 AP
+            <strong></strong>
+          </li>
+        </ul>
+        <br />
+        <p>In 5 years I would like to be in Japan</p>
+      </div>
+    </section>
+  );
 }
