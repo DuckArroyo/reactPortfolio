922610645490ca51e30d963162d0909f04d63c4e
Rafael "Duck" Arroyo <87622570+DuckArroyo@users.noreply.github.com>
2022-01-02 12:12:32 -0800

    2022 goals added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,39 +1,39 @@
-import React from "react";
-import { ExternalLink } from "react-external-link";
+import React from 'react';
+import { ExternalLink } from 'react-external-link';
 
 export default function Footer() {
   return (
-    <footer className="footer">
+    <footer className='footer'>
       <ExternalLink
-        href="https://duckarroyo.github.io/challenge2"
-        target="_blank"
+        href='https://duckarroyo.github.io/portfolio'
+        target='_blank'
       >
         HTML Portfolio
       </ExternalLink>
 
-      <ExternalLink href="https://github.com/DuckArroyo" target="_blank">
+      <ExternalLink href='https://github.com/DuckArroyo' target='_blank'>
         GitHub
       </ExternalLink>
 
-      <ExternalLink href="https://twitter.com/DevDuckArroyo" target="_blank">
+      <ExternalLink href='https://twitter.com/DevDuckArroyo' target='_blank'>
         Twitter
       </ExternalLink>
 
       <ExternalLink
-        href="https://www.linkedin.com/in/duckarroyo"
-        target="_blank"
+        href='https://www.linkedin.com/in/duckarroyo'
+        target='_blank'
       >
         LinkedIn
       </ExternalLink>
 
       <ExternalLink
-        href="https://codepen.io/DeveloperDuckArroyo"
-        target="_blank"
+        href='https://codepen.io/DeveloperDuckArroyo'
+        target='_blank'
       >
         Codepen
       </ExternalLink>
 
-      <ExternalLink href="mailto:DeveloperDuckArroyo@gmail.com">
+      <ExternalLink href='mailto:DeveloperDuckArroyo@gmail.com'>
         Email Me
       </ExternalLink>
     </footer>
