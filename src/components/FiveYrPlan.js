b879b981c728ba78796bc2cf0e62a777f456c190
Rafael "Duck" Arroyo <87622570+DuckArroyo@users.noreply.github.com>
2022-01-02 12:02:37 -0800

    2022 goals added

--- src/components/FiveYrPlan.js
+++ src/components/FiveYrPlan.js
@@ -9,32 +9,34 @@ export default function FiveYrPlan() {
         <br />
         <ul>
           <li>
-            Practice Developing
-            <strong>Goal: Create 12 applications.</strong>
+            Practice Wev Developing 
+            <strong> Goal: Create 12 applications.</strong>
           </li>
           <li>
             Learn to properly jump rope
-            <strong>Goal: 50 continuous jumps+</strong>
+            <strong>   Goal: 50 continuous double under jumps+</strong>
           </li>
           <li>
             Practice Japanese
-            <strong>Goal: create a video skit.</strong>
+            <strong>   Goal: Create a video skit.</strong>
           </li>
           <li>
             Blog
-            <strong>Goal: Write 52 blogs</strong>
+            <strong>   Goal: Write 52 blogs</strong>
           </li>
           <li>
             Vlog
-            <strong>Goal: Create 12 videos</strong>
+            <strong>   Goal: Create 12 videos</strong>
           </li>
           <li>
-            Fun goal. Reach 500,000,000 AP
-            <strong></strong>
+            Fun goal. 
+            <strong>   Reach 500,000,000 AP in Ingress</strong>
           </li>
         </ul>
         <br />
-        <p>In 5 years I would like to be in Japan</p>
+        <p><strong>Move to Japan in 2025</strong></p>
+        <p>I would like to be in Japan. I enjoy living in the United States but I am not really happy here, there fore I would like to work and travel for five to ten years. This is a big leap of faith but with good planning it is an achieveable goal.</p>
+      <p>At this time my plans are already in motion to achieve this goal. I have just completed the University of Oregon's coding bootcamp (January 2022) and became a Full Stack Web Developer. My next task is to become proficient in Japanese. </p>
       </div>
     </section>
   );
